b464bf8cd5b38cddc2d0086951d8df853bc9e3cc
hulya <hulyakarakayaa06@gmail.com>
2020-12-07 19:20:00 -0800

    create routing and install react-icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import './App.css';
-import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
-import Error from './pages/Error';
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
+
+import { Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <>
-    <Home />
-    <Rooms />
-    <SingleRoom />
-    <Error />
-
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/rooms" exact>
+          <Rooms />
+        </Route>
+        <Route path="/rooms/:slug">
+          <SingleRoom />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../images/logo.svg';
+import {FaAlignRight} from 'react-icons/fa'
+
+const Navbar = () => {
+    
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar
