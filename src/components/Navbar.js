cf9136b311d9196865640d7b69e73759e6360491
hulya <hulyakarakayaa06@gmail.com>
2020-12-07 23:42:00 -0800

    finish navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../images/logo.svg';
-import {FaAlignRight} from 'react-icons/fa'
+import { FaAlignRight } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    }
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <Link to="/">
+                        <img src={logo} alt="logo" />
+                    </Link>
+                    <button type="button" className="nav-btn" onClick={handleToggle}>
+                        <FaAlignRight className="nav-icon" />
+                    </button>
+                </div>
+
+                <ul className={isOpen ? "nav-links show-nav" : "nav-links"}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/rooms">Rooms</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
