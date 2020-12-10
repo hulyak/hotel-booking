366e40317f5fc895bd2f6f3a08cb66fd0433ed67
hulya <hulyakarakayaa06@gmail.com>
2020-12-09 18:43:34 -0800

    complete single room page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Banner from '../components/Banner'
-import Hero from '../components/Hero'
-import Services from '../components/Services'
+import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
 
 const Home = () => {
-    return (
-        <>
-          <Hero>
-            <Banner title="Luxurious rooms" subtitle="deluxe rooms starting at $399">
-            <Link to="/rooms" className="btn-primary">Our rooms</Link>
-            </Banner>
-          </Hero>
-          <Services />
-        </>
-    )
-}
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="Luxurious rooms"
+          subtitle="deluxe rooms starting at $399"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
 import { RoomContext } from "../context";
 import { Link } from "react-router-dom";
-import Hero from "../components/Hero";
+// import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import StyledHero from "../components/StyledHero";
 
 export default class SingleRoom extends Component {
   constructor(props) {
@@ -44,15 +45,60 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room;
+
+    // first image and the rest of the images
+    const [mainImg, ...restImgs] = images;
+
     return (
-      <Hero hero="roomsHero">
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            Back to rooms
-          </Link>
-        </Banner>
-      </Hero>
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+
+        <section className="single-room">
+          <div className="single-room-images">
+            {restImgs.map((image, index) => {
+              return <img key={index} src={image} alt={name} />;
+            })}
+          </div>
+
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>Details</h3>
+              <p>{description}</p>
+            </article>
+
+            <article className="info">
+              <h3>Info</h3>
+              <h6>Price: ${price}</h6>
+              <h6>Size: {size} SQFT</h6>
+              <h6>
+                Max capacity:
+                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+              </h6>
+              <h6>
+                  {pets ? "Pets allowed" : "No pets allowed"} 
+              </h6>
+              <h6>
+                  {breakfast && "free breakfast included"}
+              </h6>
+            </article>
+          </div>
+        </section>
+
+        <section className="room-extras">
+            <h6>Extras</h6>
+            <ul className="extras">
+                {extras.map((item, index) => {
+                    return <li key={index}>- {item}</li>
+                })}
+            </ul>
+        </section>
+      </>
     );
- 
   }
 }
